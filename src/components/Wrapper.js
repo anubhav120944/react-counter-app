c426b0416362337d2dc737799ed1d3f1a8bb183f
Anubhav <snehi.anubhav@tftus.com>
2022-04-30 22:25:07 +0530

    Updated useEffect

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../styles/Wrapper.module.css"
 import Counter from './Counter'
 
@@ -9,48 +9,50 @@ function Wrapper() {
     const [intervalStart, setIntervalStart] = useState();
     const [showCount, setShowCount] = useState(true);
     const [change, setChange] = useState("");
+    const [startTimer, setStartTimer] = useState(false);
 
 
     const startCounter = () => {
-        setChange("")
-        setCount(change)
-        reset();
-        setShowCount(true)
-        setIntervalStart(setInterval(() => {
-            setCount(prevState =>
-                prevState + 1
-            );
-        }, 1000))
+        setCount(Number(change));
+        setChange("");
+        setShowCount(true);
+        setStartTimer(true);
     }
     const stopCounter = () => {
-        clearInterval(intervalStart);
-    }
-
-    const reset = () => {
-        clearInterval(intervalStart);
-        setCount(Number(change));
-        setShowCount(true);
-        // setChange(count);
+        setStartTimer(false)
     }
 
     const deleteCounter = () => {
         setCount(count);
         setChange("");
         setShowCount(false);
+        setStartTimer(false)
         clearInterval(intervalStart);
 
     }
 
-
+    useEffect(() => {
+        if(intervalStart) {
+            clearInterval(intervalStart)
+        }
+        if (startTimer) {
+            setIntervalStart(setInterval(() => {
+                setCount(prevState =>
+                    prevState + 1
+                );
+            }, 1000))
+        }
+        return () => clearInterval(intervalStart)
+    }, [startTimer])
 
     return (
         <>
             <h2 className='title'>COUNTER APP</h2><br />
-            <input onChange={(e) => { setChange(e.target.value) }} value= {change} type="number" name="number" placeholder="Enter..." />
+            <input onChange={(e) => { setChange(e.target.value) }} value={change} type="number" name="number" placeholder="Enter..." />
             <h5>Enter the number from where you want to start the counter</h5>
             <div className={styles.container}>
 
-                {showCount && <h1><Counter value={count}  /></h1>}
+                {showCount && <h1><Counter value={count} /></h1>}
                 <button className={`${styles.custombtn} ${styles.btn0}`} onClick={startCounter}  ><span><b>Start</b></span></button>
                 <button className={`${styles.custombtn} ${styles.btn}`} onClick={stopCounter} ><span><b>Stop</b></span></button>
                 <button className={`${styles.custombtn} ${styles.btn2}`} onClick={deleteCounter} ><span><b>Delete</b></span></button>
