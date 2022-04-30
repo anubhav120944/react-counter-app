c3b11dd0af93801ec32dd6b6da28abc0513f13fc
Anubhav <snehi.anubhav@tftus.com>
2022-04-30 22:26:41 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Wrapper from './Components/Wrapper';
 
 function App() {
 
-  const [change, setChange] = useState(0);
+ 
 
   return (
     <div className="App">
-      <h2 className='title'>COUNTER APP</h2><br/>
-      <input onChange={(e) => { setChange(e.target.value) }} type="number" name="number" placeholder="Enter..." />
-      <h5>Enter the number from where you want to start the counter</h5>
-      <Wrapper startFrom={Number(change)} />
+      
+      <Wrapper />
     </div>
   );
 }
--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -9,65 +9,80 @@ export default class Wrapper extends Component {
         super(props);
         this.state = {
             count: 0,
-            showCount: true
+            showCount: true,
+            startTimer: false,
+            startFrom: ""
         }
     }
 
-    reset = () => {
-        clearInterval(this.interval);
-        this.setState({ count: this.props.startFrom, showCount: true });
-    }
 
     startCounter = () => {
-        this.reset();
-        this.interval = setInterval(() => {
-            this.setState(prevState => {
-                return {
-                    count: prevState.count + 1,
-                };
-            });
-        }, 1000);
+
+        this.setState({ count: Number(this.state.startFrom), startFrom: "", showCount: true, startTimer: true });
+
     }
 
     stopCounter = () => {
-        clearInterval(this.interval);
+        this.setState({ startTimer: false })
     }
 
     deleteCounter = () => {
         this.setState({
             count: 0,
-            showCount: false
+            showCount: false,
+            startTimer: false
         });
         clearInterval(this.interval);
 
     }
 
+
     componentDidMount() {
-        const { startFrom } = this.props;
-        this.setState({
-            count: startFrom
-        })
+        this.setState({ startTimer: true })
     }
+
     componentWillUnmount() {
-        this.stopCounter();
+        clearInterval(this.interval);
     }
+
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.state.count > 10) {
-            this.deleteCounter()
-            alert("Using COMPONENT_DID_UPDATE  where Counter cannot run more than 10");
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+        if (this.state.startTimer) {
+            this.interval = setInterval(() => {
+                this.setState(prevState => {
+                    return {
+                        count: prevState.count + 1,
+                    };
+                });
+            }, 1000);
         }
     }
 
+    handleChange = (e) => {
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                startFrom: e.target.value
+            }
+        })
+    }
 
     render() {
         return (
-            <div className={styles.container}>
+            <>
+                <h2 className='title'>COUNTER APP</h2><br />
+                <input onChange={this.handleChange} value={this.state.startFrom} type="number" name="number" placeholder="Enter..." />
+                <h5>Enter the number from where you want to start the counter</h5>
+                <div className={styles.container}>
 
-                {this.state.showCount && <h1><Counter value={this.state.count} /></h1>}
-                <button className={`${styles.custombtn} ${styles.btn0}`} onClick={this.startCounter} ><span><b>Start</b></span></button>
-                <button className={`${styles.custombtn} ${styles.btn}`} onClick={this.stopCounter} ><span><b>Stop</b></span></button>
-                <button className={`${styles.custombtn} ${styles.btn2}`} onClick={this.deleteCounter} ><span><b>Delete</b></span></button>
-            </div>
+                    {this.state.showCount && <h1><Counter value={this.state.count} /></h1>}
+                    <button className={`${styles.custombtn} ${styles.btn0}`} onClick={this.startCounter} ><span><b>Start</b></span></button>
+                    <button className={`${styles.custombtn} ${styles.btn}`} onClick={this.stopCounter} ><span><b>Stop</b></span></button>
+                    <button className={`${styles.custombtn} ${styles.btn2}`} onClick={this.deleteCounter} ><span><b>Delete</b></span></button>
+                </div>
+            </>
         )
     }
 }
